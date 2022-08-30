99e87652b1f34d925117c3451666da64d538adf8
AlexLug777 <109971549+AlexLug777@users.noreply.github.com>
2022-08-30 09:55:54 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# module18
+# Modul-18
+Телеграм-бот для конвертации валют. 
+Имя в телеграме @Keep_money_under_your_pillow
---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = "5516466331:AAGsFGizkdTd5sBoG7WlpfqDSyAT2tUHJUU"
+
+keys = {"Евро": "EUR", "Доллар": "USD", "Рубль": "RUB"}
